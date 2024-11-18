b58c66615215edfcc539ee56ec85be2ddaa4c59d
Noah Shoap <nshoap@vols.utk.edu>
2024-11-18 18:46:05 -0500

    feat: add canvas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function changeDisplayColor(color: string = "black")
+{
+  let display = document.querySelector("canvas");
+  if (display === null)
+    return;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  let context = display.getContext("2d");
+  if (context === null)
+    return;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  context.fillStyle = color;
+  context.fillRect(0, 0, display.width, display.height);
+}
+
+changeDisplayColor();
